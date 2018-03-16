25939c63bbfdccd54e1f6ff97bc54e8d9fdf5213
zz-fe <214464812@qq.com>
2018-03-16 19:19:49 +0800

    db

--- db.js
+++ db.js
@@ -1,20 +1,6 @@
-const mongoose = require('mongoose');
-
 let config = {
   hosts : 'localhost',
-  port : '27017',
   project : "mockSever"
 }
 
-const db = mongoose.connect(`mongodb://${config.hosts}:${config.port}/${config.project}`);
-
-const dbconnection = () => {
-  db.connection.on("open", ()=>{
-    console.log("数据库连接成功")
-  })
-  db.connection.on("error", (error)=>{
-    console.log("数据库连接失败"+ error)
-  })
-}
-
-module.exports = dbconnection
+module.exports = config
